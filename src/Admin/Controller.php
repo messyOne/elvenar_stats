13bdcf59782e2a3cd4790e511d92179af1711515
Daniel Weiss <mail@weiss-daniel.de>
2016-03-23 08:20:37 +0100

    add admin logic

--- src/Admin/Controller.php
+++ src/Admin/Controller.php
@@ -3,6 +3,8 @@
 namespace Admin;
 
 use Common\Controller\AbstractSession;
+use Common\Settings;
+use Loo\Database\DatabaseFactory;
 
 /**
  * The admin controller of the index page
@@ -26,6 +28,29 @@ class Controller extends AbstractSession
     {
         $view = $this->getViewFactory()->getHtml('Admin/view');
         $view->setLayout('admin');
+
+        if (isset($_POST['submit']) && (isset($_SESSION['logged_in']) || $_POST['password'] == Settings::getPassword())) {
+            $_SESSION['logged_in'] = true;
+
+            $sql = '
+                INSERT INTO points (date, "user", points) 
+                VALUES (:date, :user, :points)
+                ON CONFLICT (date, "user")
+                DO UPDATE SET points = EXCLUDED.points
+            ';
+            $stmt = (new DatabaseFactory())->getEntityManager()->getConnection()->prepare($sql);
+
+            $date = strtotime('today midnight');
+            foreach ($_POST as $user => $points) {
+                if (strpos($user, 'user-') !== false) {
+                    $stmt->execute([
+                        ':date' => $date,
+                        ':user' => $user,
+                        ':points' => $points,
+                    ]);
+                }
+            }
+        }
 
         $this->setView($view);
     }
